ce882479eaa74cf000be815bb1faee64cf96baf2
zhu.jiang <zhu.jiang@56qq.com>
2018-12-18 02:23:25 +0800

    thor gem

--- cli.rb
+++ cli.rb
@@ -1,69 +1,46 @@
 require 'inifile'
+require "thor"
 
 HOME = ENV['HOME']
 CONFIG = "#{HOME}/.weather"
 
-module Tty
-  module_function
+# class MyCLI < Thor
+#   desc "hello NAME", "say hello to NAME"
+#   def hello(name)
+#     puts "Hello #{name}"
+#   end
+# end
 
-  def blue
-    bold 34
-  end
+class MyCLI < Thor
+  desc "hello NAME", "say hello to NAME"
+  long_desc <<-LONGDESC
+    `cli hello` will print out a message to a person of your
+    choosing.
 
-  def red
-    bold 31
-  end
+    You can optionally specify a second parameter, which will print
+    out a from message as well.
 
-  def reset
-    escape 0
-  end
+    > $ cli hello "Yehuda katz" "Carl Lerche"
 
-  def bold(code = 39)
-    escape "1;#{code}"
-  end
-
-  def underline
-    escape "4;39"
-  end
-
-  def escape(code)
-    "\033[#{code}m" if STDOUT.tty?
+    > from: Carlerche
+  LONGDESC
+  def hello(name)
+    puts "Hello #{name}"
   end
 end
 
-class Array
-  def shell_s
-    cp = dup
-    first = cp.shift
-    cp.map { |arg| arg.gsub " ", "\\ " }.unshift(first).join(" ")
-  end
-end
+MyCLI.start(ARGV);
+
+# if !File.exists? CONFIG
+#   sudo 'touch', CONFIG
+# end
+
+# init = IniFile.load(CONFIG)
 
 
-def ohai(*args)
-  puts "#{Tty.blue}==>#{Tty.bold} #{args.shell_s}#{Tty.reset}"
-end
-
-def system(*args)
-  abort "Failed during: #{args.shell_s}" unless Kernel.system(*args)
-end
-
-def sudo(*args)
-  args.unshift("-A") unless ENV["SUDO_ASKPASS"].nil?
-  ohai "/usr/bin/sudo", *args
-  system "/usr/bin/sudo", *args
-end
-
-if !File.exists? CONFIG
-  sudo 'touch', CONFIG
-end
-
-init = IniFile.load(CONFIG)
-
-
-testInit = {}
-testInit['tenderloin'] = { 'gritty' => 'yes' }
-init.merge(testInit)
-init.each_section do |section|
-  puts "I want #{section} printed here!"
-end
+# testInit = {}
+# testInit['tenderloin'] = { 'gritty' => 'yes' }
+# init.merge(testInit)
+# init.each_section do |section|
+#   puts "I want #{section} printed here!"
+# end
